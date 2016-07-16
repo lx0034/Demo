736a83de0d29a892983cf4b410380a9bd3761089
Matrix <lx0034@foxmail.com>
2016-07-17 00:29:25 +0800

    vuetest

--- vue-practice/js/app.js
+++ vue-practice/js/app.js
@@ -1,9 +1,30 @@
-window.onload = function(){
-	alert(1);
-	new vue({
-		el: '#app',
-		data: {
-			message: 'Hello vue.js!'
-		}
-	})
-};
+new Vue({
+	el: '#app',
+	data: {
+		message: 'Hello vue.js!'
+	}
+});
+
+new Vue({
+    el: "#app2",
+    data:{
+        todos:[
+            {text: 'aaa'},
+            {text: 'bbb'},
+            {text: 'ccc'}
+        ]    
+    }
+    
+});
+
+new Vue({
+    el: "#app3",
+    data: {
+        message: "Hello Vue"
+    },
+    methods: {
+        reverseMessage: function(){
+            this.message =  this.message.split('').reverse().join();
+        }
+    } 
+});
